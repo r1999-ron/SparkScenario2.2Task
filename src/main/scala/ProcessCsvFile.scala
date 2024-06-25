658abe34e8823e62b3b6cd51dc7dc5ecf3faaed7
Ronaksengupta99 <55063393+r1999-ron@users.noreply.github.com>
2024-06-25 15:19:20 +0530

    Committing ProcessCsvFile.scala

--- src/main/scala/ProcessCsvFile.scala
+++ src/main/scala/ProcessCsvFile.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 import java.io.{File, PrintWriter}
 
 object ProcessCsv extends App {
@@ -9,6 +10,9 @@ object ProcessCsv extends App {
     .config("spark.ui.port", "4080")
     .getOrCreate()
 
+  import spark.implicits._
+
+  // Function to get list of subdirectories within a directory
   def getListOfSubDirectories(directoryName: String): Array[String] = {
     new File(directoryName)
       .listFiles
@@ -17,14 +21,27 @@ object ProcessCsv extends App {
       .map(_.getPath)
   }
 
+  // Function to perform aggregation on DataFrame
+  def performAggregation(df: org.apache.spark.sql.DataFrame): org.apache.spark.sql.DataFrame = {
+    val aggDF = df.groupBy($"_c0".alias("server_name"))  
+      .agg(
+        sum("_c3").alias("total_sum"),    
+        count("*").alias("count"),        
+        avg("_c3").alias("average")       
+      )
+    aggDF
+  }
+
+  // Function to process each folder
   def processFolder(folderPath: String): Unit = {
     println(s"Processing folder: $folderPath")
-   
+
     val df = spark.read
-      .option("header", "false") // Specifying that files do not contain headers
+      .option("header", "false") 
       .option("inferSchema", "true")
       .csv(s"$folderPath/*.csv")
-      
+
+    val aggregatedDF = performAggregation(df)
 
     val recordCount = df.count()
 
@@ -34,12 +51,21 @@ object ProcessCsv extends App {
     } finally {
       writer.close()
     }
+
+    // Show aggregated results for each server_name
+    println(s"Aggregated results for folder: $folderPath")
+    aggregatedDF.show()
   }
 
-  val baseDirectory = "/Users/ronak/Downloads/csv_files12" 
+  // Main entry point
+  val baseDirectory = "/Users/ronak/Downloads/csv_files12"
 
+  // Get list of subdirectories within the base directory
   val subFolders = getListOfSubDirectories(baseDirectory)
 
+  // Process each subfolder
   subFolders.foreach(processFolder)
+
+  // Stop the Spark session
   spark.stop()
 }
